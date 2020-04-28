f2feb9f23b71ea5bea43e9c203511c05837862df
LiMeii <mei90s.lee@gmail.com>
2020-04-28 16:33:22 +0800

    add tribonacci

--- Easy/number-4.js
+++ Easy/number-4.js
@@ -68,3 +68,51 @@ var numWays = function (n) {
 
     return n3;
 }
+
+/**
+ * 泰波那契序列 Tn 定义如下： 
+
+T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
+
+给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+
+示例 1：
+
+输入：n = 4
+输出：4
+解释：
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+示例 2：
+
+输入：n = 25
+输出：1389537
+
+ */
+
+// 暴力递归，会超时
+var tribonacci = function (n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 1;
+    return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+
+}
+
+// 枚举
+var tribonacci = function (n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 1;
+
+    var n0 = 0, n1 = 1; n2 = 1, n3 = 0;
+
+    for (let i = 3; i <= n; i++) {
+        n3 = n0 + n1 + n2;
+        n0 = n1;
+        n1 = n2;
+        n2 = n3;
+    }
+
+    return n3;
+}
